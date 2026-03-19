4abdf93f986dfd799cb8104342d5a9913f41d032
현우 강 <hyenwoo1207@gmail.com>
2026-03-19 19:38:24 +0900

    "과제2 코딩 미완성"

--- Form1.cs
+++ Form1.cs
@@ -5,15 +5,34 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+            txtMessage.KeyDown += txtMessage_KeyDown;
         }
 
         private void btn_Click(object sender, EventArgs e)
         {
+            SendMessage();
+        }
+        private void SendMessage()
+        {
             string typed_msg = txtMessage.Text;
+            if (string.IsNullOrWhiteSpace(typed_msg));
+            {
+                return;
+            }
+
             lstEcho.Items.Add(typed_msg);
             txtMessage.Clear();
             txtMessage.Focus();
             txtMessage.SelectionStart = txtMessage.Text.Length;
+        }
+
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendMessage();
+                e.SuppressKeyPress = true;
+            }
         }
 
         private void lstEcho_SelectedIndexChanged(object sender, EventArgs e)
@@ -24,6 +43,12 @@ namespace EchoMessenger
         private void txtMessage_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtMessage.Clear();
+            lstEcho.Items.Clear();
         }
     }
 }
